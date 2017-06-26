33d990e5e99e1c52616df7009c30687116bfae6a
huangyiming <huangyiminghappy@163.com>
2017-06-26 20:59:58 +0800

    初始导入

--- Disjob-jar/src/test/java/com/huangyiming/disjob/graph/JobAction.java
+++ Disjob-jar/src/test/java/com/huangyiming/disjob/graph/JobAction.java
@@ -6,16 +6,16 @@ import com.huangyiming.disjob.quence.TaskExecuteException;
 
 public class JobAction implements Runnable{
 
-	private Node<DependDisJob> dependEJob ;
+	private Node<DependDisJob> dependDisJob ;
 	private Scheduler scheduler ;
 	public JobAction(Node<DependDisJob> dependEJob,Scheduler scheduler) {
-		this.dependEJob = dependEJob ;
+		this.dependDisJob = dependEJob ;
 		this.scheduler = scheduler ;
 	}
 	public void run() {
 		try {
-			dependEJob.getVal().execute(new SchedulerParam());
-			this.scheduler.notify(dependEJob);
+			dependDisJob.getVal().execute(new SchedulerParam());
+			this.scheduler.notify(dependDisJob);
 		} catch (TaskExecuteException e) {
 			e.printStackTrace();
 		}
